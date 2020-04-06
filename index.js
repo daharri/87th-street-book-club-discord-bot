f704cea7da6bbf75d6253c94f22aa64c271a01f2
Darius <darius.harrison33@gmail.com>
2020-04-05 23:39:24 -0500

    Initial setup, save books and retrieve books

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require('fs')
+
+require('dotenv').config()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", async msg => {
+  const messageText = msg.content
+
+  if (messageText === "ping") {
+    msg.reply("Pong!")
+  } else if (messageText.includes('__addBook')) {
+    // This removes the __add command from the message
+    const bookTitle = messageText.replace(/^__addBook/i, '')
+    fs.appendFile('books.txt', bookTitle  + ',', (err) => {
+      if (err) {
+        console.log(err)
+        throw err
+      }
+    })
+    msg.reply(`Added the book - ${bookTitle} `)
+  } else if (messageText.includes('__showBooks')) {
+    const books = await fs.readFileSync('books.txt').toString().split(',');
+    console.log(books)
+    msg.reply(`All Books ${books.map(book => book).slice(0, books.length - 1)}`)
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
